a1ee1160f60ec5351c3b660145c50ecc2ce63540
ASHISH <ashish.adhikary.2003@gmail.com>
2024-07-08 12:52:02 +0600

    final commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,13 +1,29 @@
 const Hero = () => {
   return (
-    <main className="hero">
+    <main className="hero container">
       <div className="hero-content">
-        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <div>
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p className="description">
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+          </p>
+        </div>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+        <div className="more-shopping">
+          <p>Also Available On</p>
+          <div className="brand-icon">
+            <img src="images/amazon.png" alt="brand-logo" />
+            <img src="images/flipkart.png" alt="brand-logo" />
+          </div>
+        </div>
       </div>
-      <div className="hero-images">
-        YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-        SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-        SHOES.
+      <div className="hero-image">
+        <img src="/images/hero-image.png" alt="shoe-img" />
       </div>
     </main>
   );
